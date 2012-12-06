d87908b627a2c9da2f0c89b010f1d393e615d9de
deneb <contact@bblois.fr>
2012-12-06 12:04:59 +0100

    fixed lemme errors

--- src/lemme.cpp
+++ src/lemme.cpp
@@ -1,18 +1,20 @@
+#include <map>
+
 #include "lemme.hpp"
 
 Lemme::Lemme(string lemme, string docID)
 {
 	this->lemme = lemme;
 	this->nbDocAyantLemme = 0;
-	this->tableOc.inster( pair<string, int>(docID, 0));
+	this->tableOcc.insert( pair<string, int>(docID, 0));
 }	
 
 
 int Lemme::getNbOccTotal() const // Renvoie le nombre d'occurrences du lemme dans le corpus
 {
-	map<string, int> :: iterator it;
+	map<string, int> :: const_iterator it = tableOcc.begin();
 	int temp;		//valeur a retourner
-	for(it = tableOc.begin(); it!=tableOc.end(); ++it)
+	for(it = tableOcc.begin(); it!=tableOcc.end(); ++it)
 	{
 		temp += (*it).second;	//on aditionne l'ensemble des occurences du lemme.
 	}
@@ -20,16 +22,16 @@ int Lemme::getNbOccTotal() const // Renvoie le nombre d'occurrences du lemme dan
 
 bool Lemme::possedeDocID(string docID) const // Renvoie si le tableau associatif contient déjà cet identifiant de document.  
 {
-	if(tableOC.count(docID) == 0)
+	if(tableOcc.count(docID) == 0)
 		return true;
 	else return false;
 }
 
 void Lemme::ajouteDocID(string docID) // Ajoute un identifiant de document au tableau associatif.
 {
-	if(tableOC.count(docID) == 0)
+	if(tableOcc.count(docID) == 0)
 	{
-		this->tableOc.inster( pair<string, int>(docID, 0));
+		this->tableOcc.insert( pair<string, int>(docID, 0));
 		nbDocAyantLemme += 1;
 	}
 		
@@ -37,12 +39,12 @@ void Lemme::ajouteDocID(string docID) // Ajoute un identifiant de document au ta
 
 void Lemme::incrementeOccDansDocID(string docID) // Incrémente le nombre d'occurrences du lemme pour cet identifiant de document.
 {
-	if(tableOC.count(docID) == 0)
+	if(tableOcc.count(docID) == 0)
 	{
 		ajouteDocID(docID);
 	}
-	else if(tableOC.count(docID)) == 1
+	else if(tableOcc.count(docID) == 1)
 	{
-		tableOC[docID] += 1; // a tester
+		tableOcc[docID] += 1; // a tester
 	}	
 }
