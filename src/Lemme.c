a288cc06c019a33695d09e0f48c702ef071cbed5
jaennot <jean.badie@gmail.com>
2012-12-06 00:53:59 -0800

    prépa Lemme.c

--- src/Lemme.c
+++ src/Lemme.c
@@ -1,2 +1,44 @@
 #include "Lemme.h"
 
+  Lemme :: Lemme(string lemme, string docID)
+  {
+  	
+  }	
+
+  Lemme :: string getLemme() const  // Renvoie le lemme.
+  {
+  	
+  }
+  
+  
+  Lemme :: int getNbDocAyantLemme() const // Renvoie le nombre d'identifiants de documents possédant ce lemme.
+  {
+  	
+  }
+  
+  
+  Lemme :: int getNbOccDansDocID(string docID) const // Renvoie le nombre d'occurrences du lemme dans le document identifié par docID.
+  {
+  	
+  }
+  
+  
+  Lemme :: int getNbOccTotal() const // Renvoie le nombre d'occurrences du lemme dans le corpus
+  {
+  	
+  }
+  
+  Lemme :: bool possedeDocID(string docID) const // Renvoie si le tableau associatif contient déjà cet identifiant de document.  
+  {
+  	
+  }
+
+  Lemme :: void ajouteDocID(string docID) // Ajoute un identifiant de document au tableau associatif.
+  {
+  	
+  }
+  
+  Lemme :: void incrementeOccDansDocID(string docID) // Incrémente le nombre d'occurrences du lemme pour cet identifiant de document.
+  {
+  	
+  }
