7df79112dc8b1a19d0aa4b3d9e57c17b2563d05b
deneb <contact@bblois.fr>
2012-12-10 19:08:35 +0100

    worked on the main

--- src/main.cpp
+++ src/main.cpp
@@ -7,62 +7,93 @@
 
 using namespace std;
 
-void lireFichier(string path);
+vector<string> categories;
+map<string, Lemme> tableLemme;
+
+vector<string> SegmenteSelonSymbole(string str, string symbole);
+bool valideEtiquette(string s);
+void calculOccurences(string nomFichier); 
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello world !" << std::endl;
+	string nomFichier("../data/est-republicain-2002-TT.txt");
 	
-	lireFichier("../data/est-republicain-2002-TT.txt");
+	categories.push_back("ADJ");
+	categories.push_back("ADV");
+	categories.push_back("PRP");
+	categories.push_back("NOM");
+	categories.push_back("NAM");
+	categories.push_back("VER");
 	
+	
+	calculOccurences(nomFichier);
 	return 0;
 }
 
-void lireFichier(string path) 
+vector<string> SegmenteSelonSymbole(string str, string symbole)
 {
-	ifstream fichier(path.c_str());
+	vector<string> res;
+	unsigned int olPos=0, pos=string::npos;
+	
+	do 
+	{
+		pos=str.find(symbole, olPos);
+		
+		res.push_back(str.substr(olPos, pos-olPos));
+		olPos=pos+1;		
+	}while(pos != string::npos && olPos<str.length()); // evite de prendre le retour chariot
+	
+	return res;
+}
+
+bool valideEtiquette(string s)
+{
+	vector<string>::iterator it;
+	for(it=categories.begin();it!=categories.end();it++)
+		if(s==(*it))
+			return true;
+	return false;
+}
+
+void calculOccurences(string nomFichier) 
+{
+	ifstream fichier(nomFichier.c_str());
 	string tmp;
-	vector<string> v;
-	unsigned int pos=0, pos2=0, indexChar=0;
-	char caracteres[] = "&& "; 
+	vector<string> segId, segLemme, segInLemme;
+	int ligne=1;
 	
 	if(!fichier.good()) 
 	{
-		cerr << "Impossible d'ouvrir le ficher " << path << endl;
+		cerr << "Impossible d'ouvrir le ficher " << nomFichier << endl;
 		exit(1);
 	}
 	
 	//while(!fichier.eof())
 	{
 		getline(fichier, tmp);
-		pos2 = tmp.find('\t', pos);
-		//cout << "pos : " << pos << " pos2 : " << pos2 << endl;
-		if(pos2!=string::npos)
+		segId = SegmenteSelonSymbole(tmp, "\t");
+		if(segId.size()<3)
 		{
-			v.push_back(tmp.substr(pos, pos2-pos));
-			pos=pos2+1;
-		} // ID1
-		
-		pos2 = tmp.find('\t', pos);
-		//cout << "pos : " << pos << " pos2 : " << pos2 << endl;
-		if(pos2!=string::npos)
-		{
-			v.push_back(tmp.substr(pos, pos2-pos));
-			pos=pos2+1;
-		} //ID2
-		
-		while( (pos2 = tmp.find(caracteres[indexChar], pos)) != string::npos)
-		{
-			cout << "pos : " << pos << " pos2 : " << pos2 << endl;
-			v.push_back(tmp.substr(pos, pos2-pos));
-			pos=pos2+1;
-			indexChar = (indexChar+1)%3;
-			// Mot / etiquette/lemme
+			cerr << "Fichier malforme a la ligne n°" << ligne << endl;
+			exit(1);
 		}
-		v.push_back(tmp.substr(pos)); // last word
+		segLemme = SegmenteSelonSymbole(segId[2], " ");
 		
-		for(unsigned int i=0;i<v.size();i++)
-			cout << v[i] << endl;
+		for(unsigned int i=0;i< segLemme.size();i++)
+		{
+			segInLemme = SegmenteSelonSymbole(segLemme[i], "&");
+			if(segInLemme.size()<3)
+			{
+				cerr << "Lemme invalide a la " << ligne << "e ligne" << endl;
+				exit(1);
+			}
+			if(valideEtiquette(segInLemme[1]))
+			{
+				cout << segInLemme[0] << " " << segInLemme[1] << " " << segInLemme[2] << endl;
+			}
+		}
+		ligne++;
 	}
 	
+	fichier.close();
 }
