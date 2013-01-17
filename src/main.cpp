710c4be94a82f6390f7fc428b4d510c4236e9ed5
jaennot <jean.badie@gmail.com>
2013-01-17 23:40:53 +0100

    main argv used OK

--- src/main.cpp
+++ src/main.cpp
@@ -13,40 +13,38 @@ int main(int argc, char* argv[])
     Plan p("../data/metro.txt");
     std::cout << "Modelisation du METRO parisien\n\tBADIE Jean & BLOIS Benjamin" << std::endl;
     
-    if (argc < 2)
+    std::cout << "argc : " << argc << std::endl;
+    
+    if (argc <= 1)
     {
+        std::cout << "argc <= 1" <<std::endl;
         //il n'y a aucun argument on les demandes tous
         std::cout << "\nQuel est votre point de départ ?" << std::endl;
 		std::cin >> buffer;
 		depart = buffer;
     }
-    else
+    else { depart = argv[1];}
+    std::cout << "Point de départ : " << depart << std::endl;
+    if (argc <= 2)
     {
-      depart = argv[2];  
-    }
-    if (argc < 3)
-    {
+        std::cout << "argc <= 2" <<std::endl;
         //il y a au moins le point de départ on demande les autres arguments
         std::cout << "\nQuel est votre arrivee ?" << std::endl;
 		std::cin >> buffer;
 		arrivee = buffer;
     }
-    else
-    {
-        arrivee = argv[3];
-    }
+    else {arrivee = argv[2];}
+    std::cout << "Station de destination : " << arrivee << std::endl;
     
-    if (argc < 4)
+    if (argc <= 3)
     {//il y a au moins le point de départ et un point d'arrivee on demande les autres arguments
         std::cout << "\nA quel moment souhaitez-vous partir ? (creuse, normale, pointe)" << std::endl;
 		std::cin >> buffer;
 		heure = buffer;
     }
-    else
-    {
-        heure = argv[4];
-    }
-    if (argc < 5)
+    else {heure = argv[3];}
+    std::cout << "Moment de la journee : " << heure << std::endl;
+    if (argc <= 4)
     {//il y a au moins le point de départ et un point d'arrivee et heure on demande les autres arguments
         std::cout << "\nSouhaitez vous une anomalie ? (oui ou non)" << std::endl;
 		std::cin >> buffer;
@@ -56,12 +54,14 @@ int main(int argc, char* argv[])
     }
     else
     {
-        buffer = argv[5];
+        buffer = argv[4];
         if (buffer.compare("oui")) {
             anomalie = true;
         }
     }
-    std::cout << "Point de départ : " << depart << " Point de d'arrivee : " << arrivee << " type d'heure : " << heure << " avec anomalie : " << anomalie << std::endl;
+    std::cout << "Avec anomalie ? " << anomalie << std::endl;
+    
+    std::cout << "RAPPEL TRAJET\nPoint de départ : " << depart << " Point de d'arrivee : " << arrivee << " type d'heure : " << heure << " avec anomalie : " << anomalie << "\nCALCUL EN COURS" << std::endl;
 
     /*
 	std::string stDep, stArr, freq, annom;
