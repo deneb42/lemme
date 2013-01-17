bd99ef4ac0278b3f4a870eb6e86bd8b0438e035f
jaennot <jean.badie@gmail.com>
2013-01-17 20:39:24 +0100

    modif calcul des poids

--- src/transition.cpp
+++ src/transition.cpp
@@ -9,13 +9,19 @@ using namespace std;
 
 double Transition::calculerPoidsTransition(int heure)
 {
-    if ((heure <= 0 && heure > 7) || (heure <= 9 && heure > 12) || (heure <= 13 && heure > 17) || (heure <= 19 && heure > 23) )
+    random_device rd;
+    if ((heure <= 0 && heure > 7) || (heure <= 19 && heure > 23) )
     { // a ses heures faible traffic (pas de fermeture du réseau la nuit...)                             
-        return 2.0 + (calculerPoidsVoyageurs(60) / 10000.0);
+        return (double) 2.0 + (calculerPoidsVoyageurs( rd()*1000%250 )) / 10000.0;
     }
+    if ((heure <= 9 && heure > 12) || (heure <= 13 && heure > 17))
+    { //heure normale
+        return (double) 2.0 + (calculerPoidsVoyageurs(250 + rd()*1000%150) ) / 10000.0;
+    }
+    
     else //heures de pointes de la jounree
     {
-        return 2.0 + (calculerPoidsVoyageurs(650) / 10000.0); //650 capacite max d'une rame de metro parsien.
+        return (double) 2.0 + (calculerPoidsVoyageurs(400+rd()*1000%100 )) / 10000.0; //500 capacite max d'une rame de metro parsien.
     }
 }
 
@@ -24,7 +30,7 @@ double Transition::calculerPoidsVoyageurs(int nbPersonnes)
     double poids = 0;
     random_device rd;
     for (int i = 0; i < nbPersonnes; i++) {
-        poids += 50 + (rd() * 100000 % 50);
+        poids += 50 + (rd()*1000%50); //le poid des personne va de 50 à 100kg.
     }
     return poids;
 }
