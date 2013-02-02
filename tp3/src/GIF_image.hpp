126dcc1cfb8ffdfc57b0c676b3db11faef5c83e9
deneb <benjblois@hotmail.com>
2013-02-02 22:45:23 +0100

    Update tp3/src/GIF_image.hpp
    changed the anti multiple inclusion flags

--- tp3/src/GIF_image.hpp
+++ tp3/src/GIF_image.hpp
@@ -1,36 +1,33 @@
 
+#ifndef __GIF_IMAGE_HPP__
+#define __GIF_IMAGE_HPP__
 
-#ifndef ____GIF_image__
-#define ____GIF_image__
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-class GIF_image
-{
-	public:
-		GIF_image(int width, int height);
-		void setPixel(int posX, int PosY, int val);
-		void setColor( int posX, PosY, color c);
-		void setWidth(int wid);
-		void setHeight(int hei);
+	#include <iostream>
+	#include <vector>
 	
-	private:	
-		uint_8 header[13] = { 0x47, 0x49, 0x46, 0x38, 0x39, 0x61, // GIF89a
-			(uint_8)(x_size&0xff), (uint_8)(x_size>>8), // width
-			(uint_8)(y_size&0xff), (uint_8)(y_size>>8), // height
-			GLOBAL_COLOR_TABLE_FLAG | // use global color table
-			COLOR_RESOLUTION_8_BIT |  // use 8 bit per R/G/B
-			SIZE_OF_GLOBAL_COLOR_TABLE_256, // use 256 colours
-			0x00, // default background colour
-			0x00 };
+	using namespace std;
 	
-			int *pixels;
+	class GIF_image
+	{
+		public:
+			GIF_image(int width, int height);
+			void setPixel(int posX, int PosY, int val);
+			void setColor( int posX, PosY, color c);
+			void setWidth(int wid);
+			void setHeight(int hei);
 		
+		private:	
+			uint_8 header[13] = { 0x47, 0x49, 0x46, 0x38, 0x39, 0x61, // GIF89a
+				(uint_8)(x_size&0xff), (uint_8)(x_size>>8), // width
+				(uint_8)(y_size&0xff), (uint_8)(y_size>>8), // height
+				GLOBAL_COLOR_TABLE_FLAG | // use global color table
+				COLOR_RESOLUTION_8_BIT |  // use 8 bit per R/G/B
+				SIZE_OF_GLOBAL_COLOR_TABLE_256, // use 256 colours
+				0x00, // default background colour
+				0x00 };
 		
-	
-};
+			int *pixels;		
+		
+	};
 
-#endif /* defined(____GIF_image__) */
+#endif // __GIF_IMAGE_HPP__
