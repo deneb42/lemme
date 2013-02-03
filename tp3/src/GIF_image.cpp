e9b8e8c1465d34e95665d09535e23772b5738676
Jaennot <jean.badie@gmail.com>
2013-02-03 15:05:15 +0100

    maj gif_image

--- tp3/src/GIF_image.cpp
+++ tp3/src/GIF_image.cpp
@@ -8,46 +8,31 @@ GIF_image::GIF_image(int x, int y)
 {
 	this.x_size	= x;
 	this.y_size = y;
-	pixels = new int[height][width];
+	pixels.resize(y_size); // dimensionnement de la hauteur
 	
-	header = { 0x47, 0x49, 0x46, 0x38, 0x39, 0x61, // GIF89a
-		(uint_8)(x_size&0xff), (uint_8)(x_size>>8), // width
-		(uint_8)(y_size&0xff), (uint_8)(y_size>>8), // height
-		GLOBAL_COLOR_TABLE_FLAG | // use global color table
-		COLOR_RESOLUTION_8_BIT |  // use 8 bit per R/G/B
-		SIZE_OF_GLOBAL_COLOR_TABLE_256, // use 256 colours
-		0x00, // default background colour
-		0x00 };
-}
-
-GIF_image::~GIF_image()
-{
-	pixels.delete();
-}
-
-void GIF_image::setPixel(int posX, int posY, int val)
-{
-	if (val >= 0 && val <= 255)
-		pixel[posY][posX] = val;
-	else
-		return;
+	for (std::vector<vector<char> >::iterator it = pixels.begin(); it != pixels.end() ; it++) {
+		it.resize(x_size);
+	} //dimensionnement de la largeur de chaque ligne.
 	
 }
 
-//void setColor( int posX, PosY, color c)
-
-
-void GIF_image::setWidth(int wid)
+void setPixel(int posX, int posY, unsigned char val)
 {
-	this.width = wid;
-	pixels.delete();
-	pixels = new int[this.height][this.width];
+	std::vector<vector<char> >::iterator it;
+	it = pixels[posY];
+	it[posX] = val;
+	
 }
 
-
-void GIF_image::setHeight(int hei)
+void resize(int x, int y)
 {
-	this.height	= hei;
-	pixels.delete();
-	pixels = new int[this.height][this.width];
+	for (std::vector<vector<char> >::iterator it = pixels.begin(); it != pixels.end() ; it++) {
+		it.resize(x);
+	} //dimensionnement de la largeur de chaque ligne.
+
+	x_size = x;
+	
+	pixels.resize(y);
+	y_size = y;
+	
 }
--- tp3/src/GIF_image.hpp
+++ tp3/src/GIF_image.hpp
@@ -7,34 +7,39 @@
 
 using namespace std;
 
-const uint_8 GLOBAL_COLOR_TABLE_FLAG = 0x80;
-const uint_8 COLOR_RESOLUTION_8_BIT = 0x70;
-const uint_8 SORT_FLAG = 0x08;
-const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_256 = 0x07;
-const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_128 = 0x06;
-const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_64 = 0x05;
-const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_32 = 0x04;
-const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_16 = 0x03;
-const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_8 = 0x02;
-const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_4 = 0x01;
-const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_2 = 0x00;
-
 class GIF_image
 {
 	public:
-		GIF_image(int x_size, int y_size;
-		~GIF_image();
-		void setPixel(int posX, int posY, int val);
-		//void setColor( int posX, PosY, color c);
-		void setWidth(int wid);
-		void setHeight(int hei);
+		GIF_image(int x_size, int y_size);
+		void setPixel(int posX, int posY, unsigned char val);
+		void resize(int x, int y);
 	
-	private:	
-		uint_8 header[13];
 	
-		int *pixels;
-		int x_size, y_size;
-		char terminator = ";";
+		static const uint_8 GLOBAL_COLOR_TABLE_FLAG = 0x80;
+		static const uint_8 COLOR_RESOLUTION_8_BIT = 0x70;
+		static const uint_8 SORT_FLAG = 0x08;
+		static const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_256 = 0x07;
+		static const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_128 = 0x06;
+		static const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_64 = 0x05;
+		static const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_32 = 0x04;
+		static const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_16 = 0x03;
+		static const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_8 = 0x02;
+		static const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_4 = 0x01;
+		static const uint_8 SIZE_OF_GLOBAL_COLOR_TABLE_2 = 0x00;
+
+	private:
+	
+		//header
+		static const string version = "GIF89a";
+		unsigned int x_size, y_size;
+		static const unsigned char backColor = '0';
+	
+		//contenu
+		vector<vector<unsigned char> > pixels;
+		static const char fin = '0';
+	
+		//terminator
+		const char terminator = ";";
 	
 };
 
